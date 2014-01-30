af1a3e523d4aeb604d3d86591978b7814195d595
Pia Srivastava <pia_negandhi@yahoo.co>
2014-01-29 22:44:05 -0800

    adding all

---
+++ ios_twitter-master/twitter/Tweet.m
@@ -0,0 +1,29 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by Timothy Lee on 8/5/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+- (NSString *)text {
+    return [self.data valueOrNilForKeyPath:@"text"];
+}
+
+- (NSString *)from_user {
+    return [self.data valueOrNilForKeyPath:@"from_user"];
+}
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)array {
+    NSMutableArray *tweets = [[NSMutableArray alloc] initWithCapacity:array.count];
+    for (NSDictionary *params in array) {
+        [tweets addObject:[[Tweet alloc] initWithDictionary:params]];
+    }
+    return tweets;
+}
+
+@end
---
+++ ios_twitter-master/twitter/TwitterCommon.h
@@ -0,0 +1,25 @@
+//
+//  TwitterCommon.h
+//  twitter
+//
+//  Created by Timothy Lee on 8/6/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#ifndef twitter_TwitterCommon_h
+#define twitter_TwitterCommon_h
+
+// API
+#import "TwitterClient.h"
+#import <SystemConfiguration/SystemConfiguration.h>
+
+
+// Models
+#import "RestObject.h"
+#import "User.h"
+#import "Tweet.h"
+
+// Categories
+#import "NSDictionary+CPAdditions.h"
+
+#endif
